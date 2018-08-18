0567d322684c370c37e682b5311291054d30ce6c
diegogzz <diego_96gr@hotmail.com>
2018-08-18 05:00:28 -0500

    Bug fix View

--- app.js
+++ app.js
@@ -23,9 +23,6 @@ console.log = function(d) { //
   log_stdout.write(util.format(d) + '\n');
 };
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
